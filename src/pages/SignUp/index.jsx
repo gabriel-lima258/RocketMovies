c55b7345c84bf3ee0f6ad6efd941bf76892e1e76
Gabriel_Lima <gabriel58221@gmail.com>
2023-07-25 21:16:48 -0300

    implement the api in SignUp page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,39 @@
+import {useState} from 'react'; // cria um estado dinâmico de dados com back-end
 import { Container, Form, Background} from "./style";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; 
+
+import { api } from '../../services/api'; 
 
 import {Input} from '../../components/Input';
 import {Button} from "../../components/Button";
 import {FiUser, FiMail, FiLock, FiArrowLeft} from 'react-icons/fi'; 
 
 export function SignUp(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleSignUp(){
+        if(!name || !email || !password){
+           return alert("Preencha todos os campos!");
+        }
+        // chamos api para o destino users e enviar as propriedades passadas
+        api.post("/users", {name, email, password})
+        .then(() => {
+            alert("Usuário cadastrado com sucesso!"); // chama o then caso dê certo a aplicação
+            navigate("/SignIn"); 
+        })
+        .catch(error => { // catch caso dê erro
+            if(error.response){
+                alert(error.response.data.message); // pegando a mensagem de erro do back end, caso exista
+            } else {
+                alert("Não foi possível realizar o cadastro!"); // paga uma mensagem genérica
+            }
+        });
+    }
+
     return(
         <Container>
             <Form>
@@ -19,19 +47,22 @@ export function SignUp(){
                 placeholder="Nome"
                 type='text'
                 icon={FiUser}
+                onChange={e => setName(e.target.value)} // o e aciona o evento name devolvendo o valor ao banco
             />
             <Input
                 placeholder="E-mail"
                 type='text'
                 icon={FiMail}
+                onChange={e => setEmail(e.target.value)}
             />
             <Input
                 placeholder="Senha"
                 type='password'
                 icon={FiLock}
+                onChange={e => setPassword(e.target.value)}
             />
 
-            <Button title="Cadastrar" $newsave/>
+            <Button title="Cadastrar" $newsave onClick={handleSignUp}/>
             <Link to="/SignIn">
                 <FiArrowLeft/>
                 Volte para o login
