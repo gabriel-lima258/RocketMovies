8929c7ca4de03795d6dc3424b442b94760051cb4
Gabriel Lima <gabriel58221@gmail.com>
2023-07-30 16:47:13 -0300

    changed the api host

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3330" // já cria uma base quando for chamar todas requições
+    // já cria uma base quando for chamar todas requições
+    baseURL: 'https://rocketmovies-api-y99n.onrender.com' 
 });
